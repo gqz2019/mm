8f7a2eb4e1123e53c3fca85d1a2dae04dfbd4d35
gqz <gqz2019@outlook.com>
2021-03-11 10:40:39 +0800

    退出登录功能完成

--- mm_backend_management/src/main/java/com/gqz/mm/controller/UserController.java
+++ mm_backend_management/src/main/java/com/gqz/mm/controller/UserController.java
@@ -12,23 +12,32 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * @author gqz20
+ */
 @Controller
 public class UserController {
-    private UserService userService =new UserService();
+    private UserService userService = new UserService();
+
     @RequestMapping("/user/login")
     public void login(HttpServletRequest request, HttpServletResponse response) throws IOException {
         Result result;
         try {
             User user = JsonUtils.parseJSON2Object(request, User.class);
-            User loginUser=userService.login(user);
+            User loginUser = userService.login(user);
             //将user状态存入session
-            request.getSession().setAttribute(Constants.LONGIN_USER,loginUser);
-            result=new Result(true,"登陆成功",loginUser);
-            JsonUtils.printResult(response,result);
+            request.getSession().setAttribute(Constants.LONGIN_USER, loginUser);
+            result = new Result(true, "登陆成功", loginUser);
+            JsonUtils.printResult(response, result);
         } catch (Exception e) {
             e.printStackTrace();
-            JsonUtils.printResult(response,new Result(false,e.getMessage(),null));
+            JsonUtils.printResult(response, new Result(false, e.getMessage(), null));
         }
 
+    }
+
+    public void logout(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.getSession().invalidate();
+        JsonUtils.printResult(response,new Result(true,"退出成功",null));
     }
 }
